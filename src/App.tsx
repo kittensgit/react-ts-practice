dee17f5a915f44153e7a71939b28ded6fee7713b
nika <nikaonime@gmail.com>
2023-09-01 19:33:38 +0300

    object in ts, type, union type

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
 
-let pizza: string = 'salami';
-let price: number = 20;
+type Order = {
+    title: string;
+    quantity: number;
+};
 
-let pizzas: string[] = ['salami', 'cheese'];
-let numbers: number[] = [1, 2];
+const order: Order = {
+    title: 'margarita',
+    quantity: 10,
+};
+
+const orders: Order[] = [
+    {
+        title: 'margarita',
+        quantity: 10,
+    },
+    {
+        title: 'margarita',
+        quantity: 10,
+    },
+];
+
+let variable: null | number = null;
+variable = 22;
 
 function App() {
     return <div className="App">app</div>;
