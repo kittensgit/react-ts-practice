8f56ed5bf829c47a5838b158c8421ce4163e3fb5
nika <nikaonime@gmail.com>
2023-09-01 19:22:42 +0300

    variables and arrays in ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import './App.css';
 
+let pizza: string = 'salami';
+let price: number = 20;
+
+let pizzas: string[] = ['salami', 'cheese'];
+let numbers: number[] = [1, 2];
+
 function App() {
     return <div className="App">app</div>;
 }
