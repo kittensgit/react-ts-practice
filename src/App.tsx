69060d5f9992855e734ba3a8c7e0085501b06e75
nika <nikaonime@gmail.com>
2023-09-04 19:28:25 +0300

    type of Pizza, addPizza, handleSubmit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import Pizza from './models/Pizza';
 import './App.css';
 import AddPizzaForm from './components/AddPizzaForm';
 
 const App: FC = () => {
+    const [pizzasList, setPizzasList] = useState<Pizza[]>([]);
+
+    const addPizza = (newPizza: Pizza) => {
+        setPizzasList([...pizzasList, newPizza]);
+    };
+
     return (
         <div className="App">
             <div className="wrap">
                 <span className="heading">Our pizzeria</span>
-                <AddPizzaForm />
+                <AddPizzaForm addPizza={addPizza} />
             </div>
         </div>
     );
--- src/components/AddPizzaForm.tsx
+++ src/components/AddPizzaForm.tsx
@@ -1,5 +1,10 @@
 import React, { FC, useState } from 'react';
 import './styles.css';
+import Pizza from '../models/Pizza';
+
+interface AddPizzaFormProps {
+    addPizza: (newPizza: Pizza) => void;
+}
 
 const initState = {
     title: '',
@@ -7,7 +12,7 @@ const initState = {
     img: '',
 };
 
-const AddPizzaForm: FC = () => {
+const AddPizzaForm: FC<AddPizzaFormProps> = ({ addPizza }) => {
     const [newPizza, setNewPizza] = useState<{
         title: string;
         price: string;
@@ -23,6 +28,16 @@ const AddPizzaForm: FC = () => {
     };
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        const { title, price, img } = newPizza;
+        if (title && price && img) {
+            addPizza({
+                title,
+                img,
+                price: Number(price),
+                id: Date.now(),
+            });
+            setNewPizza(initState);
+        }
     };
 
     return (
